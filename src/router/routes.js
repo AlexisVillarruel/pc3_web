fbb26624ea33bf70fd84a160c800029f64fde7a2
Alexis_Jhair <alexisvillarruelj14@gmail.com>
2024-06-12 15:04:01 -0500

    loging

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,23 +1,27 @@
+import Loging from "src/components/Auth/Loging.vue";
+
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
 
   {
-    path: '/ProductsPage',
-    component: () => import('components/Auth/ProductCard.vue'),
+    path: "/ProductsPage",
+    component: () => import("components/Auth/ProductCard.vue"),
+  },
+  {
+    path: "/Loging",
+    component: () => import("components/Auth/Loging.vue"),
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
