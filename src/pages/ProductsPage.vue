b41bca9b1dbb73101fa148ba464648d0f49c783d
DiegoCh234 <21100318@ue.edu.pe>
2024-06-12 15:56:53 -0500

    Nueva correcion de page con imagenes

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,39 +1,38 @@
 <template>
-  <div class="product-page">
-    <h1>Productos en Venta</h1>
-    <div class="product-list">
-      <product-card v-for="product in products" :key="product.id" :product="product" />
+  <div class="products">
+    <h2>Listado de Productos</h2>
+    <div v-for="(producto, index) in productos" :key="index">
+      <Product :precio="producto.precio" :nombre="producto.nombre" :imagen="getImagen(producto.imagen)" />
     </div>
   </div>
 </template>
 
 <script>
-import ProductCard from '@/components/ProductCard.vue';
+import Product from '@/components/auth/Product.vue'; // Ruta al componente Product.vue
 
 export default {
   components: {
-    ProductCard
+    Product
   },
   data() {
     return {
-      products: [
-        { id: 1, name: 'Producto 1', price: '$10', image: 'public/icons/lapicero.jpg' },
+      productos: [
+        { nombre: 'Producto 1', precio: 100, imagen: 'lapicero.jpg' },
+        { nombre: 'Producto 2', precio: 150, imagen: 'planta.jpg' },
+        { nombre: 'Producto 3', precio: 80, imagen: 'consola.jpg' }
       ]
     };
+  },
+  methods: {
+    getImagen(nombreImagen) {
+      return `/icons/${nombreImagen}`;
+    }
   }
 };
 </script>
 
 <style scoped>
-.product-page {
-  max-width: 800px;
-  margin: 0 auto;
+.products {
   padding: 20px;
-}
-
-.product-list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
 }
 </style>
